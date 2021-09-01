95e3118df6239ec6be535ec0b344896d1689b854
seraad <89920975+seraad@users.noreply.github.com>
2021-09-01 16:37:08 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,91 +1,33 @@
 ﻿using System;
 
-namespace Classintro
+namespace Donguler
 {
     class Program
     {
-         static void Main (string[] args)
+        static void Main(string[] args)
         {
-            string adi = "Serhat";
-            int yas = 19;
+            // string kurs1= "Yazılım geliştirici kampı";
+            // string kurs2= "bla bla";
+            // string kurs3= "bla bla2"; e-ticaret sistemi aklına gelsin, tek tek yapamazsın.
+            // array = dizi // aşağıdaki satır dizi oluşturma //
 
-            Kurs kurs1 = new Kurs();  // kendi veri tipini yazıyormuş gibi düşün
-            kurs1.KursAdi = "C#";
-            kurs1.Egitmen = "engin demiroğ";
-            kurs1.IzlenmeOrani = 68;
+            string[] kurslar = new string[] { "Yazılım geliştirici kampı", 
+             "bla bla",
+             "bla bla2", "python"};
 
-            Kurs kurs2 = new Kurs();
-            kurs2.KursAdi = "java";
-            kurs2.Egitmen = "serhat";
-            kurs2.IzlenmeOrani = 69;
-
-            Kurs kurs3 = new Kurs();
-            kurs3.KursAdi = "python";
-            kurs3.Egitmen = "tuba";
-            kurs3.IzlenmeOrani = 80;
-
-            // Console.WriteLine(kurs1.KursAdi + " : " + kurs1.Egitmen);
-
-            Kurs[] kurslar = new Kurs[] { kurs1, kurs2, kurs3 };
-
-            foreach (var kurs in kurslar)
+            for (int i = 0; i <kurslar.Length ; i++) // i+=2 // lenght = elelman sayısı yani üstteki kurs sayısı anlamına gelir
             {
-                Console.WriteLine("Kurs Adı" + " : " + kurs.KursAdi + " Eğitmen Adı" + " : " + kurs.Egitmen + " İzlenme Sayısı" + " : " + kurs.IzlenmeOrani);
+                Console.WriteLine(kurslar [i]);
+            }
+            Console.WriteLine("for bitti");
+            foreach (string kurs in kurslar)     // dizileri tek tek dolaşır foreach
+            {
+                Console.WriteLine(kurs);
             }
 
-            Urun urun1 = new Urun();
-            urun1.UrunAdi = "Evo 8";
-            urun1.UrunLokasyonu = "Bursa";
-            urun1.UrunFiyati = 450000;
-
-            Urun[] urunler = new Urun[] { urun1 };
-
-            foreach (var urun in urunler)
-
-            { Console.WriteLine(urun1.UrunAdi + urun1.UrunFiyati + urun1.UrunLokasyonu); 
-            
-            
-            }
-
-                
-
+            Console.WriteLine("sayfa sonu - footer");
         }
-
-
-        class Kurs
-        {
-            public string KursAdi { get; set; }
-            public string Egitmen { get; set; }
-            public int IzlenmeOrani { get; set; }
-
-        }
-
-        // ödev 1: byte [0-255] sbyte [-128-127] short [-32768-32767] ushort, int, uint, long, ulong, float, double, decimal, char, string, boolean
-        // ödev 2: ternary operator: ? ile gösterilir, bir bakıma if else işlevi görür.
-        // ödev 3: switch komutu aslen kontrol komutudur. girilen veri kontrol edilir. 
-        // ödev 4: while komutu ona atanan değere kadar bir döngüyü çalıştırır. değere ulaşıldığında döngüyü durdurur.
-        // ödev 5
+         
         
-             
-        
-            
-
-
-             class Urun
-        {
-            public string UrunAdi { get; set; }
-            public string UrunLokasyonu { get; set; }
-            public int UrunFiyati { get; set; }
-
-        }
-
     }
 }
-
-
-
-
-
-       
-
-
